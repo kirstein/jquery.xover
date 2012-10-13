ded6dd0dd15a49cb3e946afd5908e7c0b3ae08e8
Mikk Kirstein <mikk.kirstein@zerotech.ee>
2012-10-13 23:38:37 +0300

    reworked solution

--- js-text-xover.js
+++ js-text-xover.js
@@ -4,11 +4,12 @@
             if (typeof value === "undefined") {
                 throw new Error("Value can't be undefined");
             }
-            this.el = el;
-            this.$el = $(el);
+            this.el      = el;
+            this.$el     = $(el);
             this.options = options;
-            this.value = $.trim(value);
-            this.change = {
+            this.value   = $.trim(value);
+            this.text    = $.trim(this.$el.text());
+            this.change  = {
                 'curr': null,
                 'prev': null,
                 'diff': null
@@ -16,67 +17,105 @@
         };
 
     Xover.prototype.init = function() {
-        var root = this;
+        if (this.text !== this.value) {
+            this.writeEl();
+            this.writeTransformEl();
+            this.manipulateEl();
+        }
+    };
 
-        if (this.isValueChanged()) {
-            this.manipulateValues(function() {
-                root.options.call(this, this.change.prev, this.change.diff);
+    Xover.prototype.writeTransformEl = function() {
+        var firstChange = this.getFirstChangePos(),
+            $el, $appendElement, i;
+
+        $appendElement = this.$el.children('span:not(.'+ this.options.elChangeClass + '):nth-child('+ firstChange +')');
+
+        for (i = firstChange; i < this.value.length; i += 1) {
+            $el = $('<span/>', {
+                'class' : this.options.elClass + ' ' + this.options.elChangeClass,
+                'text'  : this.value[i],
+                'style' : 'display:none'
             });
+            
+            if ($appendElement.length) {
+                $el.insertAfter($appendElement);
+            } else {
+                this.$el.empty();
+                $el.prependTo(this.$el);
+            }
+
+            $appendElement = $el;
+        }                
+    };
+
+    Xover.prototype.manipulateEl = function() {
+        var root        = this,
+            firstChange = this.getFirstChangePos(),
+            changeElems;
+
+        // Animate temporary elements and change them to main ones.
+        // Remove old main elements.
+        function toggleClassesAndRem() {
+            $(this).remove();
+            animateNewElements();
+        }
+
+        function animateNewElements() {
+            root.$el.children('span.' + root.options.elChangeClass).fadeIn(root.options.speed, function() {
+                $(this).toggleClass(root.options.elChangeClass).toggleClass(root.options.elClass);
+            }); 
+        }
+
+        if (typeof firstChange !== 'undefined') {
+            
+            // List all elements that come after changed element.
+            // Animate main elements.
+
+            changeElems = this.$el.children('span.' + this.options.elClass + ':nth-child(' + firstChange + ')')
+                              .nextAll().filter(':not(.' + this.options.elChangeClass + ')');
+
+            if (changeElems.length) {
+                changeElems.fadeOut(this.options.speed, toggleClassesAndRem);
+            } else {
+                animateNewElements();
+            }
+
+        } else {
+            // Debug
+            console.error(this);
         }
     };
 
-    Xover.prototype.isValueChanged = function() {
-        var prevVal = this.$el.text() * 1,
-            newVal = this.value * 1,
-            notNumeric = isNaN(prevVal) && isNaN(newVal);
-
-        if (!notNumeric && prevVal !== newVal) {
-            this.change.prev = prevVal;
-            this.change.diff = newVal - prevVal;
-            this.change.curr = newVal;
-
-            return true;
-        }
-
-        return false;
+    /**
+     * Returns the first changed character in the comparson to old and new value.
+     */
+    Xover.prototype.getFirstChangePos = function() {
+         for (var i = 0; i < this.value.length; i += 1) {
+             if (this.text.length < i || this.value[i] !== this.text[i]) {
+                return i;
+             }
+         }
     };
 
-    Xover.prototype.buildElement = function() {
-        var currValue = this.change.curr.toString(),
-            prevValue = this.change.prev.toString(),
-            maxLength = Math.max(currValue.length, prevValue.length),
-            $el       = $(),
-            i;
+    Xover.prototype.writeEl = function() {
+        var text = $.trim(this.$el.text()),
+            $el  = $();
 
-        // Create empty spaces in front of real values
-        for (i = 0; i < (maxLength - currValue.length); i += 1) {
+        for (i = 0; i < text.length; i += 1) {
             $el = $el.add('<span/>', {
                 'class' : this.options.elClass,
-                'html'  : this.options.emptyVal
-            });
-        }
-        // Wrap each letter to span
-        for (i = 0; i < currValue.length; i += 1) {
-            $el = $el.add('<span/>', {
-                'class' : this.options.elClass,
-                'text'  : currValue[i]
+                'text'  : text[i]
             });
         }
 
-        return $el;
+        this.$el.html($el);
     };
-
-    Xover.prototype.manipulateValues = function() {
-        var $newEl = this.buildElement();
-        this.$el.html($newEl);
-    };
-
     $.fn.xover = function(value, options) {
         var root = this;
 
         if (typeof val === 'object' && typeof opt === 'undefined') {
             options = value;
-            value = option.value;
+            value   = option.value;
         }
         options = $.extend({}, $.fn.xover.options, options);
         return this.each(function() {
@@ -86,8 +125,10 @@
 
     $.fn.xover.options = {
         value    : undefined,
-        change   : function(prevVal, diff) {},
+        change   : function() {},
         elClass  : 'xover-el',
-        emptyVal : '&nbsp;'
+        elChangeClass : 'xover-change',
+        emptyVal : '&nbsp;',
+        speed    : 'slow'
     };
 })(jQuery);
